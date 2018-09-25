537cb856f823b3c97f9e1d0e5e737fac0385118a
Aditya Hase <aditya@adityahase.com>
2018-09-25 20:34:17 +0530

    Migrate Payment Vouchers

--- app.py
+++ app.py
@@ -261,6 +261,7 @@ def get_vouchers(tally_company, start_date, end_date):
                 "Journal": transform_journal_voucher,
                 "Sales": transform_sales_voucher,
                 "Purchase": transform_purchase_voucher,
+                "Payment": transform_payment_voucher,
             }
             function = voucher_type_mapping.get(voucher.VOUCHERTYPENAME.string)
             if function:
@@ -331,6 +332,28 @@ def transform_purchase_voucher(xml):
         accounts.append(account)
     voucher = {
         "voucher_type": "Purchase",
+        "guid": xml.GUID.string,
+        "posting_date": xml.DATE.string,
+        "accounts": accounts,
+    }
+    return voucher
+
+
+def transform_payment_voucher(xml):
+    accounts = []
+    for ledger_entry in xml.find_all("ALLLEDGERENTRIES.LIST"):
+        account = {
+            "account": ledger_entry.LEDGERNAME.string,
+            "is_party": ledger_entry.ISPARTYLEDGER.string == "Yes",
+        }
+        amount = Decimal(ledger_entry.AMOUNT.string)
+        if amount > 0:
+            account["credit_in_account_currency"] = str(abs(amount))
+        else:
+            account["debit_in_account_currency"] = str(abs(amount))
+        accounts.append(account)
+    voucher = {
+        "voucher_type": "Payment",
         "guid": xml.GUID.string,
         "posting_date": xml.DATE.string,
         "accounts": accounts,
